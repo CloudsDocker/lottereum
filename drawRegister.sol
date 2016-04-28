57d62f015441e4a5e9fe8835bdba0644be97c3a9
glynnbird <glynn.bird@gmail.com>
2016-04-28 13:26:01 +0000

    draw registry

---
+++ drawRegister.sol
@@ -0,0 +1,28 @@
+contract drawRegister {
+   address owner;
+   uint public numDraws;
+   struct drawData {
+      uint drawDate;
+      address eth_address;
+   }
+   mapping(uint => drawData) public draws;
+ 
+
+
+   function drawRegister()  {
+      numDraws = 0;
+      owner = msg.sender;
+   }
+
+   function addDraw (address _eth_address) {
+      if (msg.sender != owner) throw;
+      draws[numDraws] = drawData(now, _eth_address);
+      numDraws += 1;      
+   }
+
+   function getLatestDraw () returns (address latest) {
+      if (numDraws == 0) throw;
+      latest = draws[numDraws-1].eth_address;
+   }
+
+}
