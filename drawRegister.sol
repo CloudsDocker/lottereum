5ea23e711e537dc53425058e393f2d5d216b1221
danielmermelstein <daniel@windermere.home>
2016-05-21 10:15:06 +0100

    the register now communicates with the draws!!

--- drawRegister.sol
+++ drawRegister.sol
@@ -1,3 +1,14 @@
+contract drawInterface {
+
+  function getPot() constant returns (uint) {
+  }
+ 
+  function buyTicket (uint _guess) {
+  }
+
+}
+
+
 contract drawRegister {
    address owner;
    uint public numDraws;
@@ -12,6 +23,17 @@ contract drawRegister {
    function drawRegister()  {
       numDraws = 0;
       owner = msg.sender;
+   }
+
+   function getPot() constant returns (uint pot) {
+     drawInterface draw = drawInterface(getLatestDraw());
+     pot = draw.getPot(); 
+   }
+
+   function buyTicket(uint _guess)  {
+     drawInterface draw = drawInterface(getLatestDraw());
+     draw.buyTicket.value(msg.value)(_guess);
+
    }
 
    function addDraw (address _eth_address) {
